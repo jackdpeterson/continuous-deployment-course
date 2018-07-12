946bc4e04c7e97d1a342c7b0e14a9686d1756d5a
Jack Peterson <jack.peterson@gmail.com>
2018-07-11 23:57:15 -0700

    add puppet configuration script

--- configure_puppet.sh
+++ configure_puppet.sh
@@ -6,12 +6,8 @@ sudo apt-get update
 sudo apt-get install puppet-agent librarian-puppet
 sudo ln -s /opt/puppetlabs/puppet/bin/puppet /usr/local/bin/puppet
 
-
 ## update puppet modules
 cd puppet && librarian-puppet install
-
-## add local modules
-cp -r local_modules/site_reliability_engineering modules/
 
 ## apply the puppet plan
 sudo puppet apply /home/ubuntu/continuous-deployment-course/puppet/manifests/awscdimage72.pp --modulepath=/home/ubuntu/continuous-deployment-course/puppet/modules/ --hiera_config=/home/ubuntu/continuous-deployment-course/puppet/aws.yaml
